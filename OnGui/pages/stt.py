d37644325e6400ed25f00a305a29bf654b09192f
buzzlee21 <nemogun21@gmail.com>
2025-11-17 16:40:32 +0900

    gui developing

--- OnGui/pages/stt.py
+++ OnGui/pages/stt.py
@@ -14,7 +14,7 @@ import numpy as np
 import whisper
 import sounddevice as sd
 
-from pro_update import ProUpdateClass
+from pages.pro_update import ProUpdateClass
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from db_connect import get_connection
