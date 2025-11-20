68be6eb323de3e219a40dfd6e6a9eb8468b41abc
TNTTNTTTN <isugkgkgk@gmail.com>
2025-11-20 21:48:29 +0900

    feat: Update MarketCoreManager, pinky_msgs, CartStateManager, SensorManager

--- CartStateManager/CartStateManager/CartStateManager.py
+++ CartStateManager/CartStateManager/CartStateManager.py
@@ -1,28 +1,41 @@
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Twist
-import math
+from geometry_msgs.msg import Twist, Pose2D
+from std_msgs.msg import Int32, Float32
+from nav_msgs.msg import Odometry
 from rcl_interfaces.msg import SetParametersResult
 from std_msgs.msg import Float64
-from pinky_msgs.msg import StateControl
-from pinky_msgs.msg import RobotState
+from pinky_msgs.msg import RobotState, PoseOrder, Humanpos, ItemReq
 
 from .tcp_socket import TCPSocket
 from .config import *
 from .state_machine import *
 
+import math
 import time
 import threading
 import queue
 import struct
-
+import os
 
 class CartStateManager(Node):
     def __init__(self):
         super().__init__('CartStateManager')
 
+        robot_domain_id = os.environ.get('ROS_DOMAIN_ID')
+        self.robot_id = int(robot_domain_id) - 31
         self.state_machine = StateMachine(STATE_TABLE, 0)
         self.user_id = -1
+        self.state_code_timer = 0.1
+        self.robot_status_timer = 5
+
+        self.battery_percentage = 0.0
+        self.posx = 0.0
+        self.posy = 0.0
+        self.destin_posx = 0.0
+        self.destin_posy = 0.0
+        self.destin_theta = 0.0
+        self.user_id = 0
 
         self.perception_manager_queue = queue.Queue(maxsize=1)
         self.interface_manager_queue = queue.Queue(maxsize=1)
@@ -34,8 +47,132 @@ class CartStateManager(Node):
         self.perception_manager_socket_start()
         self.interface_manager_socket_start()
 
+        
+        self.robot_status_publisher  = self.create_publisher(
+            RobotState,
+            'robot_state',
+            10
+        )
+
+        self.human_pose_publisher = self.create_publisher(
+            Humanpos,
+            'human_pos',
+            10
+        )
+
+        self.item_destination_req_publisher = self.create_publisher(
+            ItemReq,
+            'item_req',
+            10
+        )
+
+        self.user_check_req_publisher = self.create_publisher(
+            ItemReq,
+            'user_check',
+            10
+        )
+
+        self.user_check_resp_subscriber = self.create_subscription(
+            ItemReq,
+            'user_resp',
+            self.user_check_resp_callback,
+            10
+        )
+
+        self.battery_status_subscriber = self.create_subscription(
+            Float32,
+            'battery/present',
+            self.battery_status_callback,
+            10
+        )
+
+        self.filtered_odom_subscriber = self.create_subscription(
+            Odometry,
+            "odometry/filtered",
+            self.filtered_odom_callback,
+            10
+        )
+
+        self.position_order_subscriber = self.create_subscription(
+            PoseOrder,
+            'pose_order',
+            self.position_order_callback,
+            10
+        )
+
+        self.task_subscriber = self.create_subscription(
+            Int32,
+            'task_state',
+            self.task_callback,
+            10
+        )
+
         for t in self.thread_list:
             t.start()
+        self.perception_manager_sending_timer = self.create_timer(self.state_code_timer, self.perception_manager_sending)
+        self.interface_manager_sending_timer = self.create_timer(self.state_code_timer, self.interface_manager_sending)
+        self.robot_status_sending_timer = self.create_timer(self.robot_status_timer, self.robot_status_sending)
+
+    def user_check_resp_callback(self, msg : ItemReq):
+        if msg.cid != self.robot_id:
+            return
+        else:
+            if (msg.item):
+                self.get_logger().info("USER CHECKED")
+            else:
+                self.user_id = -1
+                self.get_logger().error("USER ID NOT IN ID")            
+
+    def task_callback(self, msg : Int32):
+        task_status = msg.data
+        if (task_status == 1):
+            self.state_machine.recv_event("DEFAULT")
+
+    def position_order_callback(self, msg : PoseOrder):
+        
+        print(self.robot_id)
+        if (self.robot_id == msg.cid):
+            self.state_machine.recv_event("DEFAULT")
+            self.destin_posx = msg.posx
+            self.destin_posy = msg.posy
+            self.destin_theta = msg.theta
+            print(self.destin_posx, self.destin_posy, self.destin_theta)
+
+    def filtered_odom_callback(self, msg : Odometry):
+        self.posx, self.posy = msg.pose.pose.position.x, msg.pose.pose.position.y
+
+    def battery_status_callback(self, msg : Float32):
+        self.state_machine.recv_event(msg.data)
+        self.battery_percentage = msg.data
+
+    def robot_status_sending(self):
+        msg = RobotState()
+
+        current_state = self.state_machine.get_state()
+        msg.cid = self.robot_id
+        msg.id = self.user_id
+        msg.stid = current_state.state_id
+        msg.bat = self.battery_percentage
+        msg.posx = self.posx
+        msg.posy = self.posy
+
+        self.robot_status_publisher.publish(msg)
+
+    def interface_manager_sending(self):
+        current_state = self.state_machine.get_state()
+        if (self.interface_manager_socket.connected == True):
+            try:
+                self.interface_manager_socket.send_data(struct.pack("<i",current_state.state_id))
+            except:
+                pass
+
+    def perception_manager_sending(self):
+        current_state = self.state_machine.get_state()
+        if (self.perception_manager_socket.connected == True):
+            try:
+                self.perception_manager_socket.send_data(struct.pack("<i",current_state.state_id))
+            except:
+                pass
 
     def perception_manager_socket_start(self):
         self.perception_manager_socket.init_socket()
@@ -45,6 +182,12 @@ class CartStateManager(Node):
 
         recv_thread = threading.Thread(target=self.perception_manager_socket.recv_data, args=(self.perception_manager_queue,), daemon=True)
         self.thread_list.append(recv_thread)
+
+        recv_callback_thread = threading.Thread(target=self.perception_manager_recv_callback, daemon=True)
+        self.thread_list.append(recv_callback_thread)
+
+        health_callback_thread= threading.Thread(target=self.perception_manager_socket.client_health_check, daemon=True)
+        self.thread_list.append(health_callback_thread)
 
     def interface_manager_socket_start(self):
         self.interface_manager_socket.init_socket()
@@ -58,10 +201,15 @@ class CartStateManager(Node):
         recv_callback_thread = threading.Thread(target=self.interface_manager_recv_callback, daemon=True)
         self.thread_list.append(recv_callback_thread)
 
+        health_callback_thread= threading.Thread(target=self.interface_manager_socket.client_health_check, daemon=True)
+        self.thread_list.append(health_callback_thread)
+
+
     def perception_manager_recv_callback(self):
         while True:
             if self.perception_manager_queue.full():
                 data = self.perception_manager_queue.get_nowait()
+                #!@
                 print(data)
                 self.perception_manager_data_check(data)
             else:
@@ -82,6 +230,23 @@ class CartStateManager(Node):
         if (struct.unpack("<i",data[0:4])[0] != CARTSTATEMANAGER_ID):
             self.get_logger().info(f"WRONG DATA DESTINATION FROM PERCEPTIONMANAGER")
             return
+        
+        if (struct.unpack("<i",data[8:12])[0] == PERCEPTIONMANAGER_FUNCTION1_ID):
+            user_checked = struct.unpack(PERCEPTIONMANAGER_FUNCTION_1, data[12:])[0]
+            if user_checked:
+                self.state_machine.recv_event("DEFAULT")
+
+
+        if (struct.unpack("<i",data[8:12])[0] == PERCEPTIONMANAGER_FUNCTION2_ID):
+            cam_data = struct.unpack(PERCEPTIONMANAGER_FUNCTION_2, data[12:])
+            msg = Humanpos()
+
+            msg.camid = cam_data[2]
+            msg.posx = cam_data[3]
+            msg.posy = cam_data[4]
+            msg.distance = cam_data[5]
+
+            self.human_pose_publisher.publish(msg)
 
     def interface_manager_data_check(self, data):
         if (struct.unpack("<i",data[0:4])[0] != CARTSTATEMANAGER_ID):
@@ -90,18 +255,28 @@ class CartStateManager(Node):
         
         if (struct.unpack("<i",data[8:12])[0] == INTERFACEMANAGER_FUNCTION1_ID):
             self.user_id = struct.unpack(INTERFACEMANAGER_FUNCTION_1, data[12:])[0]
-            print(self.user_id)
+
+            send_data = struct.pack(INTERFACECOMMON+CARTSTATEMANAGER_FUNCTION_1, 
+                                    INTERFACEMANAGER_ID, 1, CARTSTATEMANAGER_FUNCTION1_ID, True)
+            self.interface_manager_socket.send_data(send_data)
         
         elif (struct.unpack("<i",data[8:12])[0] == INTERFACEMANAGER_FUNCTION2_ID):
-            data = struct.unpack(INTERFACEMANAGER_FUNCTION_2, data[12:])[0]
-            if data[0] == 0:
-                pass
-            elif data[1] == 1:
+            data = struct.unpack(INTERFACEMANAGER_FUNCTION_2, data[12:])
+            if data[0] == 1:
+                print(f"Item ID : {data[1]}")
+
+                msg = ItemReq()
+                msg.cid = self.robot_id
+                msg.item = data[1]
+
+                self.item_destination_req_publisher.publish(msg)
+
+            elif data[0] == 2:
+                print("MODE SELECTION")
                 pass
             else:
                 self.get_logger().info(f"WRONG COM CODE FROM INTERFACEMANAGER")
         pass
-
 
 def main(args=None):
     rclpy.init(args=args)
--- SensorManager/SensorManager/SensorManager.py
+++ SensorManager/SensorManager/SensorManager.py
@@ -1,23 +1,45 @@
 import rclpy
+
 from rclpy.node import Node
+from tf_transformations import quaternion_from_euler
+
 from std_msgs.msg import Float32
 from sensor_msgs.msg import Imu
 from nav_msgs.msg import Odometry
-import pinkylib
+from pinky_msgs.msg import Encoder
+
+import math
+from .imu import IMU
+from .battery import Battery
+
+WHEEL_RAD = 0.028
+PULSE_PER_ROT = 4096 
+WHEEL_BASE = 0.0961
+RPM2RAD = 2 * math.pi / 60
+CIRCUMFERENCE = 2 * math.pi * WHEEL_RAD
 
 IMU_FRAMEID = "imu"
 ODOM_FRAMEID = "odom"
+ODOM_CHILDID = "odometry/filtered"
 BATTERY_FRAMEID = "battery/present"
+ENCODER_FRAMEID = "encoder"
 
 class SensorManager(Node):
     def __init__(self):
         super().__init__('SensorManager')
-        self.battery = pinkylib.Battery()
-        self.Imu = pinkylib.Imu()
+        self.battery = Battery()
+        self.Imu = IMU()
 
-        
+        self.last_time = 0
+        self.last_encoder_l = 0
+        self.last_encoder_r = 0
+        self.theta = 0
+        self.x = 0
+        self.y = 0
+
         self.battery_timer_period = 5.0
         self.imu_timer_period = 0.1
+
         self.percentage_timer = self.create_timer(self.battery_timer_period, self.battery_percentage_callback)
         self.imu_timer = self.create_timer(self.imu_timer_period, self.imu_sensor_callback)
 
@@ -40,8 +62,55 @@ class SensorManager(Node):
         )
 
         self.encoder_subscriber = self.create_subscription(
-            
+            Encoder,
+            ENCODER_FRAMEID,
+            self.encoder_callback,
+            10
         )
+
+    def encoder_callback(self, msg):
+        current_time = self.get_clock().now()
+        dt = (current_time - self.last_time).nanoseconds / 1e9
+        if dt <= 0: return
+
+        rpm_l = msg.rpm_l
+        rpm_r = msg.rpm_r 
+        encoder_l = msg.pos_raw_l
+        encoder_r = msg.pos_raw_r
+
+        delta_l = encoder_l - self.last_encoder_l
+        delta_r = -(encoder_r - self.last_encoder_r)
+        
+        self.last_encoder_l = encoder_l
+        self.last_encoder_r = encoder_r
+
+        dist_l = (delta_l / PULSE_PER_ROT) * CIRCUMFERENCE
+        dist_r = (delta_r / PULSE_PER_ROT) * CIRCUMFERENCE
+
+        delta_distance = (dist_r + dist_l) / 2.0
+        delta_theta = (dist_r - dist_l) / WHEEL_BASE
+        
+        self.theta += delta_theta
+        self.x += delta_distance * math.cos(self.theta)
+        self.y += delta_distance * math.sin(self.theta)
+        
+        v_x = delta_distance / dt if dt > 0 else 0.0
+        vth = delta_theta / dt if dt > 0 else 0.0
+
+        self._publish_odometry(current_time, v_x, vth)
+
+        self.last_time = current_time
+
+    def _publish_odometry(self, current_time, v_x, vth):
+        odom_msg = Odometry()
+        odom_msg.header.stamp = current_time.to_msg()
+        odom_msg.header.frame_id = ODOM_FRAMEID
+        odom_msg.child_frame_id = ODOM_CHILDID
+        odom_msg.pose.pose.position.x, odom_msg.pose.pose.position.y = self.x, self.y
+        q = quaternion_from_euler(0, 0, self.theta)
+        odom_msg.pose.pose.orientation.x, odom_msg.pose.pose.orientation.y, odom_msg.pose.pose.orientation.z, odom_msg.pose.pose.orientation.w = q
+        odom_msg.twist.twist.linear.x, odom_msg.twist.twist.angular.z = v_x, vth
+        self.odom_raw_publisher.publish(odom_msg)
 
     def imu_sensor_callback(self):
         t = Imu()
